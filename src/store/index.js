5dd12e06196f29ad0ef25195861679243be07016
iampooran <pooransen50@gmail.com>
2023-02-09 17:15:26 +0530

    Working with Multiple Slices

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,12 @@
 import {createStore} from 'redux'
 import { createSlice, configureStore } from '@reduxjs/toolkit'
 
-const intialState = { counter : 0, showCounter: true}
+const intialCounterState = { counter : 0, showCounter: true}
+
 
 const counterSlice = createSlice({
     name: 'counter',
-    initialState: intialState,
+    initialState: intialCounterState,
     reducers:{
         increment(state) {
             state.counter++;
@@ -22,14 +23,27 @@ const counterSlice = createSlice({
     }
 });
 
+const intialAuthState = { isAuthenticated : false}
 
+const authSlice = createSlice({
+    name: 'authentication',
+    intialState: intialAuthState,
+    reducers:{
+        login(state){
+            state.isAuthenticated = true
+        },
+        logout(state){
+            state.isAuthenticated = false
+        }
+    }
+})
 
 
 const store = configureStore({
-    reducer: counterSlice.reducer
+    reducer: {counter: counterSlice.reducer, auth: authSlice.reducer}
 });
 
 export const counterActions = counterSlice.actions;
-
+export const authActions = authSlice.actions;
 
 export default store;
